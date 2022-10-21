ba1d36518eb8a441f0ecfa6201b9a6eb33abba2f
Makar <53338728+Slohn@users.noreply.github.com>
2022-10-21 23:57:52 +0400

    фиксы

---
+++ JobTask/ShapePrototype.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTask
+{
+    public class ShapePrototype
+    {
+        public double Square(Shape shape) 
+        {
+            return shape.Square();
+        }
+    }
+}
