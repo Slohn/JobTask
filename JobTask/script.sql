de6318967154778f6e43a0989ad2a095d0ddede8
Makar <53338728+Slohn@users.noreply.github.com>
2022-10-21 23:40:52 +0400

    sql

---
+++ JobTask/script.sql
@@ -0,0 +1,3 @@
+﻿select Products.Name,categories.Name from Products 
+LEft Join ProductsToCategories on Products.Id = ProductsToCategories.ProductId 
+Left Join categories on ProductsToCategories.Categoryid = categories.Id
